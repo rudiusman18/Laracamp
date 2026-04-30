062adffd8a9f2e1e8eb07ed4ade63a02ace57129
Rudi Usman <rudiusmanprogrammer@gmail.com>
2026-04-30 18:10:37 +0700

    Update success-checkout.blade.php

--- resources/views/success-checkout.blade.php
+++ resources/views/success-checkout.blade.php
@@ -6,9 +6,9 @@
 <div class="flex flex-col justify-center items-center mt-[100px]">
     <img class="w-[401px] h-[434px]" src="{{ asset('images/checkout-success.png') }}" alt="checkout image">
 
-    <p>WHAT A DAY!</p>
-    <p>Berhasil Checkout</p>
-    <button>My DAshboard</button>
+    <p class="text-[#31B380] font-semibold text-[16px] mt-[100px]">WHAT A DAY!</p>
+    <p class="font-semibold text-[30px] mt-[8px]">Berhasil Checkout</p>
+   <a href="/"><button class="bg-[#7839F3] text-white rounded-full text-medium text-[16px] py-2 px-10 mt-[20px]" >My Dashboard</button></a>
 </div>
 
 
